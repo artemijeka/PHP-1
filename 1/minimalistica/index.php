64bf8ddd74d87ff816351e18a44f645ffaa7b0eb
Artemije <artem.kuznecov.samara@gmail.com>
2017-10-26 22:49:16 +0400

    добавил переменную h1 и title

--- 1/minimalistica/index.php
+++ 1/minimalistica/index.php
@@ -1,26 +1,32 @@
+<?php 
+	$h1 = "minimalistica";
+	$title = "minimalistica";
+	// $today = getdate();
+	// print_r($today);
+?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 	<meta http-equiv="content-type" content="text/html;charset=utf-8" />
 	<meta name="author" content="Luka Cvrk (www.solucija.com)" />
 	<link rel="stylesheet" href="css/main.css" type="text/css" />
-	<title>minimalistica</title>
+	<title><?php echo $title; ?></title>
 </head>
 <body>
 	<div id="content">
-		<h1>minimalistica</h1>
+		<h1><?php echo $h1; ?></h1>
 		
 		<ul id="menu">
 			<li><a href="#">home</a></li>
 			<li><a href="#">archive</a></li>
 			<li><a href="#">contact</a></li>
 		</ul>
-	
+
 		<div class="post">
 			<div class="details">
 				<h2><a href="#">Nunc commodo euismod massa quis vestibulum</a></h2>
 				<p class="info">posted 3 hours ago in <a href="#">general</a></p>
-			
+
 			</div>
 			<div class="body">
 				<p>Nunc eget nunc libero. Nunc commodo euismod massa quis vestibulum. Proin mi nibh, dignissim a pellentesque at, ultricies sit amet sapien. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel lorem eu libero laoreet facilisis. Aenean placerat, ligula quis placerat iaculis, mi magna luctus nibh, adipiscing pretium erat neque vitae augue. Quisque consectetur odio ut sem semper commodo. Maecenas iaculis leo a ligula euismod condimentum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Ut enim risus, rhoncus sit amet ultricies vel, aliquet ut dolor. Duis iaculis urna vel massa ultricies suscipit. Phasellus diam sapien, fermentum a eleifend non, luctus non augue. Quisque scelerisque purus quis eros sollicitudin gravida. Aliquam erat volutpat. Donec a sem consequat tortor posuere dignissim sit amet at ipsum.</p>
