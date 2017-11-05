0056b0461084adec6a959a9bfca5a801936946b6
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-05 22:45:37 +0400

    Добавил label в index для input а сами input скрыл

--- 4/HW/public_html/index.php
+++ 4/HW/public_html/index.php
@@ -9,16 +9,18 @@
 	<p>
 		ТЗ:
 		<br>
-		Создать галерею фотографий. Она должна состоять всего из одной странички, на которой пользователь видит все картинки в уменьшенdном виде и форму для загрузки нового изображения. При клике на фотографию она должна открыться в браузере в новой вкладке. Размер картинок можно ограничивать с помощью свойства width. При загрузке изображения необходимо делать проверку на тип и размер файла.
+		Создать галерею фотографий. Она должна состоять всего из одной странички, на которой пользователь видит все картинки в уменьшенном виде и форму для загрузки нового изображения. При клике на фотографию она должна открыться в браузере в новой вкладке. Размер картинок можно ограничивать с помощью свойства width. При загрузке изображения необходимо делать проверку на тип и размер файла.
 	</p>
 	<div class="gallery">
 		<?include "../templates/gallery.php"; ?>
 	</div>
 	<form enctype="multipart/form-data" action="../engine/get_image.php" method="POST">
 		<input type="hidden" name="MAX_FILE_SIZE" value="8388608" />
-		<input type="file" name="userImage" autofocus multiple accept="image/*,image/jpeg" />
+		<label class="buttonForUploads" for="userImage">Выбрать изображение.</label><br>
+		<input hidden="" type="file" id="userImage" name="userImage" autofocus multiple accept="image/*,image/jpeg" />
 		<br>
-		<input type="submit" value="Загрузить" />
+		<label class="buttonForSubmit" for="submit">Загрузить.</label><br>
+		<input type="submit" hidden="" id='submit' value="Загрузить" />
 	</form>
 </body>
 </html>
