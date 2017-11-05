b7a067b8e00c4821c4f9f46f0c9d5bc60f026a4a
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-05 22:22:14 +0400

    файл для загрузки изображений

--- 4/HW/engine/get_image.php
+++ 4/HW/engine/get_image.php
@@ -1,23 +1,17 @@
 <?
 
-$uploaddir = '../data/uploads/';
-$uploadfile = $uploaddir.basename($_FILES['userImage']['name']);
+$uploadDir = '../data/uploads/';
+// $uploadFfile = $uploadDir.basename($_FILES['userImage']['name']);
+$userImage = $uploadDir.$_FILES['userImage']['name'];
 
-$userImage = $uploaddir.$_FILES['userImage']['name'];
 if(copy($_FILES['userImage']['tmp_name'],$userImage))
   echo "Файл загружен!";
 else
   echo "Возникла ошибка при загрузке!";
 
-// if (move_uploaded_file($_FILES['userimage']['tmp_name'], $uploadfile)) {
-// 	echo "Файл корректен и был успешно загружен.\n";
-// } else {
-// 	echo "Файл не подходит для загрузки!\n";
-// }
-
 echo "<pre>";
 
-echo 'Некоторая отладочная информация:';
+echo 'Отладочная информация:';
 print_r($_FILES);
 
 echo "</pre>";
