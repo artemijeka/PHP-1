77fec808939648230e64d37e42841aa97d9383d0
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-05 23:05:42 +0400

    еще подправил стили

--- 4/HW/engine/get_image.php
+++ 4/HW/engine/get_image.php
@@ -2,12 +2,18 @@
 
 $uploadDir = '../data/uploads/';
 // $uploadFfile = $uploadDir.basename($_FILES['userImage']['name']);
+$types = array('image/gif', 'image/png', 'image/jpeg', 'image/pjpeg');
+$size = 8388608;
 $userImage = $uploadDir.$_FILES['userImage']['name'];
 
-if(copy($_FILES['userImage']['tmp_name'],$userImage))
-  echo "Файл загружен!";
-else
+if (!in_array($_FILES['userImage']['type'], $types)) {
+  echo 'Недопустимый тип файла. Допустимо загружать только изображения: *.gif, *.png, *.jpg';
+} else if (copy($_FILES['userImage']['tmp_name'], $userImage)) {
+  $succes = "Файл загружен!";
+}
+else {
   echo "Возникла ошибка при загрузке!";
+}
 
 echo "<pre>";
 
