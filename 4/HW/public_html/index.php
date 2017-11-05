15c4b4421d802d7c49c9ed7c3bc7d324e528ed15
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-05 22:22:45 +0400

    исправил ошибки

--- 4/HW/public_html/index.php
+++ 4/HW/public_html/index.php
@@ -6,13 +6,13 @@
 </head>
 <body>
 	<h1>ДЗ по PHP-1 урок 4 - задание #1.</h1>
-	<blockquote>
+	<p>
 		ТЗ:
 		<br>
 		Создать галерею фотографий. Она должна состоять всего из одной странички, на которой пользователь видит все картинки в уменьшенdном виде и форму для загрузки нового изображения. При клике на фотографию она должна открыться в браузере в новой вкладке. Размер картинок можно ограничивать с помощью свойства width. При загрузке изображения необходимо делать проверку на тип и размер файла.
-	</blockquote>
+	</p>
 	<div class="gallery">
-		<?include "../engine/get_image.php"; ?>
+		<?include "../templates/gallery.php"; ?>
 	</div>
 	<form enctype="multipart/form-data" action="../engine/get_image.php" method="POST">
 		<input type="hidden" name="MAX_FILE_SIZE" value="8388608" />
@@ -20,6 +20,5 @@
 		<br>
 		<input type="submit" value="Загрузить" />
 	</form>
-	<?print_r($_FILES); ?>
 </body>
 </html>
