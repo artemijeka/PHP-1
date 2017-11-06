f821b4de52b8a9515ee9ecd6c47f3e664acf8071
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-06 19:45:08 +0400

    исправил ошибку не name надо было проверять а type

--- 4/HW/engine/get_image.php
+++ 4/HW/engine/get_image.php
@@ -3,13 +3,13 @@
 $uploadDir = '../data/uploads/';
 // $uploadFfile = $uploadDir.basename($_FILES['userImage']['name']);
 // Массив с допустимыми расширениями файлов для загрузки.
-$types = array('image/gif', 'image/png', 'image/jpeg', 'image/pjpeg', 'image/*');
+$types = array('image/gif', 'image/png', 'image/jpeg', 'image/pjpeg', 'image/svg');
 // Допустимый размер файла.
 $size = 5242880;
 $userImage = $uploadDir.$_FILES['userImage']['name'];
 
-// Проверка на расширение загружаемого файла.
-if (!in_array($_FILES['userImage']['name'], $types)) {
+// Проверка на тип загружаемого файла.
+if (!in_array($_FILES['userImage']['type'], $types)) {
   echo 'Недопустимый тип файла. Допустимо загружать только изображения: gif, png, jpg';
 } else if ($_FILES['userImage']['size'] > $size) {
 	echo "Вы пытаетесь загрузить изображение больше 5Мб!";
