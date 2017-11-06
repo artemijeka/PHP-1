d5add1ba906d482558eaa341649ef2429fe430e0
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-06 19:32:59 +0400

    исправил ошибку проверки типа файла

--- 4/HW/engine/get_image.php
+++ 4/HW/engine/get_image.php
@@ -3,13 +3,13 @@
 $uploadDir = '../data/uploads/';
 // $uploadFfile = $uploadDir.basename($_FILES['userImage']['name']);
 // Массив с допустимыми расширениями файлов для загрузки.
-$types = array('*.gif', '*.png', '*.jpeg', '*.jpg', '*.jpeg');
+$types = array('image/gif', 'image/png', 'image/jpeg', 'image/pjpeg', 'image/*');
 // Допустимый размер файла.
 $size = 5242880;
 $userImage = $uploadDir.$_FILES['userImage']['name'];
 
 // Проверка на расширение загружаемого файла.
-if (in_array($_FILES['userImage']['name'], $types)) {
+if (!in_array($_FILES['userImage']['name'], $types)) {
   echo 'Недопустимый тип файла. Допустимо загружать только изображения: gif, png, jpg';
 } else if ($_FILES['userImage']['size'] > $size) {
 	echo "Вы пытаетесь загрузить изображение больше 5Мб!";
