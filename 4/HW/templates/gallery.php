c9469e210ef176e9d154fdd599c71329f56f631e
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-06 23:25:49 +0400

    Исправил ошибку возникшую из-за недопонимания

--- 4/HW/templates/gallery.php
+++ 4/HW/templates/gallery.php
@@ -1,28 +1,21 @@
 <?
-	// create_thumbnail($path, $save, $width, $height);
+	// Подключение функции create_thumbnail($path, $save, $width, $height);
 	require_once('../engine/image_reduction.php');
 
 	$arrayNamesImages = scandir('../data/uploads/');
 	
 	// $i=2 потому что в массиве имена файлов начинаются с 2 индекса (первые два индекса имеют значения: . и ..)
 	for ($i=2; $i<count($arrayNamesImages); $i++) {
-		// Логика перемещения изображения на следущую строку.
-		// if ($i===5||$i===8||$i===11||$i===14||$i===17) {
-		// 	echo "<br>";
-		// }
-		print_r($arrayNamesImages);
-
-		//---
+		// Переменные для функции уменьшения изображений.
 		$path = "../data/uploads/$arrayNamesImages[$i]";
-		// $save = "../data/thumbnail/$arrayNamesImages[$i]";
-		$save = false;
+		$save = "../data/thumbnails/$arrayNamesImages[$i]";
 		$width = 250;
 		$height = 250;
+		// Вызов функции уменьшения изображений.
 		$thumbnail = create_thumbnail($path, $save, $width, $height);
 		//---
-
 		echo "<a href='../data/uploads/$arrayNamesImages[$i]' target='_blank'>";
-		echo "<div style='background-image: url(../data/thumbnail/$thumbnail); background-size: cover; background-position: center;'; class='gallery__image'></div>";
+		echo "<div style='background-image: url(../data/thumbnails/$arrayNamesImages[$i]); background-size: cover; background-position: center;'; class='gallery__image'></div>";
 		echo "</a>";
 	}
 
