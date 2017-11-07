913aa4d7c88eab984234dac723faf9f0ae2063e3
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-07 22:54:58 +0400

    Сделал require_once get_image.php на главной странице вместо выполнения в форме атрибутом action="";

--- 4/HW/public_html/index.php
+++ 4/HW/public_html/index.php
@@ -13,9 +13,9 @@
 		Создать галерею фотографий. Она должна состоять всего из одной странички, на которой пользователь видит все картинки в уменьшенном виде и форму для загрузки нового изображения. При клике на фотографию она должна открыться в браузере в новой вкладке. Размер картинок можно ограничивать с помощью свойства width. При загрузке изображения необходимо делать проверку на тип и размер файла. (Готово.)
 	</p>
 	<div class="gallery">
-		<?include "../templates/gallery.php"; ?>
+		<? include "../templates/gallery.php"; ?>
 	</div>
-	<form enctype="multipart/form-data" action="../engine/get_image.php" method="POST">
+	<form enctype="multipart/form-data" action="" method="POST">
 		<p class='gallery__title'>Вы можете загрузить изображение размером не больше 5Мб.</p>
 		<!-- Для ограничения размера файла можно использовать. -->
 		<!-- <input type="hidden" name="MAX_FILE_SIZE" value="5242880" /> -->
@@ -24,6 +24,7 @@
 		<br>
 		<label class="buttonForSubmit" for="submit">Загрузить.</label><br>
 		<input type="submit" hidden="" id='submit' value="Загрузить" />
+		<p class='gallery__title'>Информация: <? require_once('../engine/get_image.php');	?></p>
 	</form>
 <!-- 	<?
 		// echo "<pre>";
