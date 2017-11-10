f3a4df5360d5d65e69df73ccd838b8e68d221435
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-10 07:08:18 +0400

    ...

--- 5/HW/templates/gallery.php
+++ 5/HW/templates/gallery.php
@@ -1,9 +1,9 @@
 <?
 	// Подключение функции create_thumbnail($path, $save, $width, $height);
 	require_once('../engine/image_reduction.php');
+	// Подключил файл запроса к базе данных.
+	require('../engine/query_to_db.php');
 
-	$arrayNamesImages = scandir('../data/uploads/');
-	
 	// $i=2 потому что в массиве имена файлов начинаются с 2 индекса (первые два индекса имеют значения: . и ..)
 	for ($i=2; $i<count($arrayNamesImages); $i++) {
 		// Переменные для функции уменьшения изображений.
@@ -13,7 +13,7 @@
 		$height = 250;
 		// Вызов функции уменьшения изображений.
 		$thumbnail = create_thumbnail($path, $save, $width, $height);
-		//---
+		// Вывод на экран ссылок и галереи изображений.
 		echo "<a href='../data/uploads/$arrayNamesImages[$i]' target='_blank'>";
 		echo "<div style='background-image: url(../data/thumbnails/$arrayNamesImages[$i]); background-size: cover; background-position: center;'; class='gallery__image'></div>";
 		echo "</a>";
