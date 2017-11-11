f0c586eb3dcbd8a5eec67cd702abf51fd7709d05
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-11 12:43:51 +0400

    переменные заменил на подходящие и счетчик i вернул на 0

--- 5/HW/templates/gallery.php
+++ 5/HW/templates/gallery.php
@@ -1,34 +1,35 @@
 <?
+
+// Запрорсить файл сортировки и вывода изображенией в порядке популярности.
+require_once('../engine/query_sort_by_views.php');
+
 // Подключение файла обрабатывающего загрузку изображения на сервер. -->
 require_once('../engine/get_image.php');
 
 // Подключение функции create_thumbnail($path, $save, $width, $height);
 require_once('../engine/image_reduction.php');
-
-// Сканирую папку с изображениями и создаю массив имен изображений.
-$arrayNamesImages = scandir('../data/uploads/');
 	
 // $i=2 потому что в массиве имена файлов начинаются с 2 индекса (первые два индекса имеют значения: . и ..)
-	for ($i=2; $i<count($arrayNamesImages); $i++) {
-
+	for ($i=0; $i<count($arrayNamesImagesSortByHits); $i++) {
+		// var_dump($arrayNamesImagesSortByHits);
 		// Переменные для функции уменьшения изображений.
-		$path = "../data/uploads/$arrayNamesImages[$i]";
-		$save = "../data/thumbnails/$arrayNamesImages[$i]";
-		$width = 250;
-		$height = 250;
+		$path = "../data/uploads/$arrayNamesImagesSortByHits[$i]";
+		$save = "../data/thumbnails/$arrayNamesImagesSortByHits[$i]";
+		$width = 200;
+		$height = 200;
 
 		// Вызов функции уменьшения изображений.
 		$thumbnail = create_thumbnail($path, $save, $width, $height);
 
-		// Вывод на экран ссылок и галереи изображений.
-		// echo "<a href='../data/uploads/$arrayNamesImages[$i]' target='_blank'>";
-		echo "<a href='./big_image.php/?currentNameImage=$arrayNamesImages[$i]' target='_blank'>";
-		echo "<div style='background-image: url(../data/thumbnails/$arrayNamesImages[$i]); background-size: cover; background-position: center;'; class='gallery__image'></div>";
+		// Вывод на экран ссылок и галереи изображений в порядке популярности..
+		echo "<a href='./big_image.php/?currentNameImage=$arrayNamesImagesSortByHits[$i]' target='_blank'>";
+		echo "<div style='background-image:url(../data/thumbnails/$arrayNamesImagesSortByHits[$i]); background-size:cover; background-position:center;' class='gallery__image'></div>";
 		echo "</a>";
 	}
+
 
 	// echo "<pre>";
 	// echo 'Массив имен файлов в папке uploads:';
 	// print_r($arrayNamesImages);
 	// echo "</pre>";
-	
+
