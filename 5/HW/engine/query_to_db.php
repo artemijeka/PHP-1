10f811ab448744313e2036a7b7cbf956a542f13e
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-11 12:33:49 +0400

    коннект в отдельный файл

--- 5/HW/engine/query_to_db.php
+++ 5/HW/engine/query_to_db.php
@@ -1,9 +1,5 @@
 <?
-	// Затребовать файл с конфигурацией.
-	require_once('conf.php');
-
-	// Произвести подключение к базе данных.
-	$connect = mysqli_connect(HOST, USER, PASSWORD, DATABASE) or die ("Невозможно подключиться к БД");
+	require_once('connect_to_db.php');
 
 	// Шаблон добавления информации о файле присвоил переменной.
 	$query = "INSERT INTO lesson_5(name, way, size) VALUES('$nameImage', '$uploadDir', '$sizeImage');";
