56979c32009d41f4120e639dd8ea10e848a766f0
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-11 07:41:55 +0400

    добавил занос размера изображения в базу данных

--- 5/HW/engine/query_to_db.php
+++ 5/HW/engine/query_to_db.php
@@ -1,12 +1,12 @@
 <?
 	// Затребовать файл с конфигурацией.
 	require_once('conf.php');
-	
+
 	// Произвести подключение к базе данных.
 	$connect = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
 
 	// Шаблон добавления информации о файле присвоил переменной.
-	$query = "INSERT INTO lesson_5(name, way) VALUES('$nameImage', '$uploadDir');";
+	$query = "INSERT INTO lesson_5(name, way, size) VALUES('$nameImage', '$uploadDir', '$sizeImage');";
 
 	// Делаю mysqli запрос который выполняет шаблон $query.
 	$queryToTable = mysqli_query($connect, $query);
