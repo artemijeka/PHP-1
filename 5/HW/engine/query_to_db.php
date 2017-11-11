fb65ad456665dd656dc1f8bd5f5128eb95c88aa4
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-11 10:02:06 +0400

    немного дописал

--- 5/HW/engine/query_to_db.php
+++ 5/HW/engine/query_to_db.php
@@ -3,7 +3,7 @@
 	require_once('conf.php');
 
 	// Произвести подключение к базе данных.
-	$connect = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
+	$connect = mysqli_connect(HOST, USER, PASSWORD, DATABASE) or die ("Невозможно подключиться к БД");
 
 	// Шаблон добавления информации о файле присвоил переменной.
 	$query = "INSERT INTO lesson_5(name, way, size) VALUES('$nameImage', '$uploadDir', '$sizeImage');";
@@ -11,3 +11,5 @@
 	// Делаю mysqli запрос который выполняет шаблон $query.
 	$queryToTable = mysqli_query($connect, $query);
 	
+	// Закрыть соединение.
+	mysqli_close($connect);
