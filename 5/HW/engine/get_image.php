5172d4bcdb8aee85cf68df2116215fbbde15c814
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-11 07:29:53 +0400

    удалил закомментированный код проверки на тип и размер в php и сделал проверку в html

--- 5/HW/engine/get_image.php
+++ 5/HW/engine/get_image.php
@@ -5,13 +5,8 @@ function refresh() {
 	exit;
 }
 
+// Дирректория загрузки файлов.
 $uploadDir = '../data/uploads/';
-// $uploadFile = $uploadDir.basename($_FILES['userImage']['name']);
-// Массив с допустимыми расширениями файлов для загрузки.
-// $types = array('image/gif', 'image/png', 'image/jpeg', 'image/pjpeg', 'image/svg', 'image/jpg');
-
-// Допустимый размер файла.
-// $size = 5242880;
 
 // Переменная хранит имя текущего загружаемого файла.
 $nameImage = $_FILES['userImage']['name'];
@@ -19,14 +14,6 @@ $nameImage = $_FILES['userImage']['name'];
 // Дирректория загрузки и название файла.
 $userImageDirectoryFile = $uploadDir.$nameImage;
 
-// Проверка на тип загружаемого файла.
-// if (!in_array($_FILES['userImage']['type'], $types)) {
-  // echo 'Недопустимый тип файла. Допустимо загружать только изображения: gif, png, jpg<br>';
-// Проверка на размер файла.
-// } else if ($_FILES['userImage']['size'] > $size) {
-	// echo "Вы пытаетесь загрузить изображение больше 5Мб!<br>";
-// Если текущее имя загружаемого файла не равно одному из существующих.
-// } else if (!in_array($nameImage, $arrayNamesImages)) {
 // Если текущее имя загружаемого файла не равно одному из существующих.
 if (!in_array($nameImage, $arrayNamesImages)) {
 		// То если копирование файла из папки temp в папку ../data/uploads/с текущим именем файла произошло успешно.
