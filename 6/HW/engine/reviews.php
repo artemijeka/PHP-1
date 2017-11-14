323752ea3fea4c6b02fca334236f9d2ae18a2639
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-14 07:25:25 +0400

    закомментировал подключение к базе

--- 6/HW/engine/reviews.php
+++ 6/HW/engine/reviews.php
@@ -1,8 +1,8 @@
 <?
 // Соединение с базой данных.
-require_once('connect_to_db.php');
+#require_once('connect_to_db.php');
 // Шаблон добавления отзыва в таблицу lesson_6_reviews
-$templateToAddReviewToTable = "INSERT INTO lesson_6_reviews(userName, email, review, currentDate) VALUES('$userName', '$email', '$review', '$currentDate');";
+#$templateToAddReviewToTable = "INSERT INTO lesson_6_reviews(userName, email, review, currentDate) VALUES('$userName', '$email', '$review', '$currentDate');";
 // Получаем имя пользователя.
 $userName = $_POST['userName'];
 var_dump($userName);
