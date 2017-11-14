238c338e5be35061bc94ea602c50bbe99d768fb2
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-14 21:29:01 +0400

    еще немного описал получение данных из формы

--- 6/HW/engine/reviews.php
+++ 6/HW/engine/reviews.php
@@ -1,8 +1,16 @@
 <?
-// Соединение с базой данных.
-#require_once('connect_to_db.php');
-// Шаблон добавления отзыва в таблицу lesson_6_reviews
-#$templateToAddReviewToTable = "INSERT INTO lesson_6_reviews(userName, email, review, currentDate) VALUES('$userName', '$email', '$review', '$currentDate');";
 // Получаем имя пользователя.
-$userName = $_POST['userName'];
-var_dump($userName);
+$userName = $_POST["userName"]; 
+// Получаем email пользователя.
+$email = $_POST["userEmail"];
+// Получаем отзыв пользователя.
+$userReview = $_POST["userReview"];
+// Получаем дату отзыва пользователя.
+$currentDate = date('j F o G:i');
+// Заносим в базу данных данные о отзыве.
+
+// Соединение с базой данных.
+include_once('connect_to_db.php');
+// Шаблон добавления отзыва в таблицу lesson_6_reviews
+$templateToAddReviewToTable = "INSERT INTO lesson_6_reviews(userName, userEmail, userReview, currentDate) VALUES('$userName', '$userEmail', '$userReview', '$currentDate');";
+mysqli_query($connect, $templateToAddReviewToTable);
