d8d037d35876ca5b8d63d28d4027d6bd4d1c2f71
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-18 09:23:54 +0400

    ...не знаю что с чем связать

--- 7/HW7_a_classmate/modules/cart.php
+++ 7/HW7_a_classmate/modules/cart.php
@@ -3,18 +3,19 @@
 $arrayCart = array();
 //Если POST случился.
 if ($_POST) {
+//  Если в массиве есть данный id.
+  if (array_key_exists($_POST['id'], $arrayCart)) {
+//    То добавить кол-во товаров к этому id.   
+  	// $arrayCart[$_POST['id']]+= $_POST['quantity'];
+  	setcookie('id', +$_POST['quantity']);
+  }
 //  Если в массиве нет данного id.
-	if (!in_array($arrayCart, $_POST['id'])) {
+	if (!array_key_exists($_POST['id'], $arrayCart)) {
 //    Добавить ключ в массив это id товара со значением кол-ва.
 		$arrayCart[$_POST['id']] = $_POST['quantity'];
 	}	
-//  Если в массиве есть данное id.
-  if (in_array($arrayCart, $_POST['id'])) {
-//    То добавить кол-во товаров к этому id.   
-  	$arrayCart[$_POST['id']]+= $_POST['quantity'];
-  }
 //Создать куки корзины которому присвоить получившийся массив.
-$cookieCart = setcookie('cookieCart', $arrayCart);
+// $cookieCart = setcookie('cookieCart', $arrayCart);
 }
 
-var_dump($cookieCart);
+// var_dump($arrayCart);
