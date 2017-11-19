00798272c8cbf95524041db900b87bd8d6238f95
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-19 15:27:44 +0400

    дз новый проект питомника шауцеров

---
+++ 8/HW8/public/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Питомник шнауцеров Монинг Стар</title>
+	<meta name="description" content="Питомник шнауцеров Монинг Стар">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+</head>
+<body>
+	<header>
+		<h1>Добро пожаловать в питомник шнауцеров "Монинг Стар"</h1>
+		<form method="POST" action="">
+			<h2>Регистрация:</h2>
+			<label for="login" title="Никнэйм английскими буквами">Логин&nbsp;&nbsp;</label>
+			<input type="text" name="login" id='login' placeholder="Nickname">
+			<br>
+			<br>
+			<label for="pass">Пароль</label>
+			<input type="password" name="pass" id='pass' placeholder="Пароль">
+		</form>
+	</header>
+
+	<main>
+		
+	</main>
+	
+	<footer>
+		
+	</footer>
+</body>
+</html>
