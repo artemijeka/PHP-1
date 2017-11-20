80c05ac1fdde3b2d76bd236d914370ae48b97da1
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-20 20:56:42 +0400

    конфигурационный файл

---
+++ 8/HW8/config/conf.php
@@ -0,0 +1,10 @@
+<?
+define('HOST', 'localhost');
+define('MYSQL_LOGIN', 'artem');
+define('MYSQL_PASS', 'admin');
+define('MYSQL_DB', 'MORNING_STAR');
+define('MYSQL_TABLE', 'users');
+
+// Соль с перцем:
+define('SALT', ':Hs|6`c&^$&*)5]#%f:');
+define('PAPPER', '-D8&c#|5G$u4P}c%3G-');
