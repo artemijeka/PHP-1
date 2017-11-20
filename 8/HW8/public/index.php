a4e71b786715ddec2b180395875378d97e8fc8b0
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-20 20:58:07 +0400

    файл вывода начальной страницы

--- 8/HW8/public/index.php
+++ 8/HW8/public/index.php
@@ -1,3 +1,9 @@
+<?
+require_once('../config/conf.php');
+require_once('../engine/models.php');
+require_once('../engine/registration.php');
+// session_start();
+?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
@@ -6,25 +12,38 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <body>
 	<header>
 		<h1>Добро пожаловать в питомник шнауцеров "Монинг Стар"</h1>
-		<form method="POST" action="">
-			<h2>Регистрация:</h2>
-			<label for="login" title="Никнэйм английскими буквами">Логин&nbsp;&nbsp;</label>
-			<input type="text" name="login" id='login' placeholder="Nickname">
-			<br>
-			<br>
-			<label for="pass">Пароль</label>
-			<input type="password" name="pass" id='pass' placeholder="Пароль">
+		<form method="POST" action="" class='registration'>
+			<fieldset class='registration__fieldset'>
+				<legend class='registration__legend'>Регистрация:</legend>
+				<label for="login" title="Никнэйм английскими буквами" class="<?=$class_login;?>">Логин</label>
+				<br>
+				<input type="text" name="login" id='login' placeholder="Nickname">
+				<br>
+				<br>
+				<label for="pass" class="<?=$class_pass;?>"><?=$label_pass_content;?></label>
+				<br>
+				<input type="password" name="pass" id='pass' placeholder="Пароль">
+				<br>
+				<br>
+				<label for="pass2" class="<?=$class_pass2;?>"><?=$label_pass2_content;?></label>
+				<br>
+				<input type="password" name="pass2" id='pass2' placeholder="Пароль">
+				<br>
+				<br>
+				<input type="submit" name='submitted' value="Подтверждаю">
+			</fieldset>
 		</form>
 	</header>
 
 	<main>
 		
 	</main>
-	
+
 	<footer>
 		
 	</footer>
