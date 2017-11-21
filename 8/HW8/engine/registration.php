792525262755825e1f1a84d045642c64432712c3
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-21 23:07:34 +0400

    добавил имя при регистрации и пытаюсь сделать разделение логина и реигстрации

--- 8/HW8/engine/registration.php
+++ 8/HW8/engine/registration.php
@@ -1,23 +1,33 @@
 <?
+// Контроллер.
 	$login = (string)htmlspecialchars(strip_tags(md5(SALT.$_POST['login'].PAPPER)));
+	$name = $_POST['userName'];
 	$pass = (string)htmlspecialchars(strip_tags(md5(PAPPER.$_POST['pass'].SALT)));
 	$pass2 = (string)htmlspecialchars(strip_tags(md5(PAPPER.$_POST['pass2'].SALT)));
 	$pass_null = (string)htmlspecialchars(strip_tags(md5(PAPPER.''.SALT)));
 	$login_null = (string)htmlspecialchars(strip_tags(md5(SALT.''.PAPPER)));
+	$name_null = '';
 	$submitted = $_POST['submitted'];
 	$date = gmdate('j.n.o G:i:s');
 
 	$label_pass_content = 'Пароль';
 	$label_pass2_content = 'Повторите пароль';
 	$label_login_content = 'Логин';
+	$label_name_content = 'Ваше полное имя';
 	$legend_content = 'Регистрация:';
 	$class_legend = 'registration_legend';
 
 // ДОБАВИТЬ ПРОВЕРКУ НА ОТПРАВКУ ФОРМЫ И НЕ ОБРАБАТЫВАТЬ СЛЕДУЮЩИЙ КОД ЕСЛИ НЕБЫЛА НАЖАТА КНОПКА $submitted !!!
+
 	if ( $login===$login_null ) {
 		$class_login = 'registration__login_error';
 		$label_login_content = 'Укажите логин!';
-	} 
+	}
+
+	if ( $name===$name_null ) {
+		$class_name = 'registration__name_error';
+		$label_name_content = 'Укажите имя!';
+	}  
 
 	if ( $pass===$pass_null ) {
 		$class_pass = 'registration__pass_error';
@@ -29,7 +39,7 @@
 		$label_pass2_content = 'Пароли не совпадают!';
 	} 
 
-	if ($login!==$login_null && $pass!==$pass_null && $pass2!==$pass_null) {
+	if ($login!==$login_null && $name!==$name_null && $pass!==$pass_null && $pass2!==$pass_null) {
 		// Если в базе данных есть такой логин. 
 		if (login_is_busy_or_not($login)) {
 			// То говорим: "Введенный логин занят!"
@@ -42,7 +52,13 @@
 			// $class_login = '';
 			$legend_content = 'Вы зарегистрировались!';
 			$class_legend = 'registration__legend_red';
-			db_user_registration($login, $pass2, $date);
+			db_user_registration($login, $pass2, $date, $name);
+			
+			// ????????????????????????????????????
+			// Установка cookies.
+			setcookie('login', $login, 2592000);
+			setcookie('pass', $pass, 2592000);
+			// refresh();
+			var_dump($_COOKIE);
 		}
 	}
-	
