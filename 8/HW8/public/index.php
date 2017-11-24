901669eab5e05ebb91c483186f0ec432132828b0
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-24 22:33:51 +0400

    индекс доработал

--- 8/HW8/public/index.php
+++ 8/HW8/public/index.php
@@ -1,22 +1,15 @@
 <?
-// error_reporting(E_ERROR);
+error_reporting(E_ERROR);
 require_once('../config/conf.php');
 require_once('../models/models.php');
 // session_start();
 ?>
 <!DOCTYPE html>
 <html lang="ru">
-<head>
-	<title>Питомник шнауцеров Монинг Стар</title>
-	<meta name="description" content="Питомник шнауцеров Монинг Стар">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico">
-	<link rel="stylesheet" type="text/css" href="css/style.css">
-</head>
+	<? require_once('../templates/head.php'); ?>
 <body>
+	<h1>Добро пожаловать в питомник шнауцеров "Монинг Стар"</h1>
 	<header>
-		<h1>Добро пожаловать в питомник шнауцеров "Монинг Стар"</h1>
 		<? require_once('../engine/user_is_logged_or_not.php'); ?>
 	</header>
 
