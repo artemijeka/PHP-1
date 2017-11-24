2631fa9a979e2102a34f3c7d0016e68d3c34c537
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-24 22:31:11 +0400

    перенес head в шаблоны

---
+++ 8/HW8/templates/head.php
@@ -0,0 +1,8 @@
+<head>
+	<title>Питомник шнауцеров Монинг Стар</title>
+	<meta name="description" content="Питомник шнауцеров Монинг Стар">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="shortcut icon" type="image/x-icon" href="../favicon.ico">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+</head>
