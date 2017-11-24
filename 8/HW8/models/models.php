80f50f4b903eeedc7dbee840adfba9ff56e0fae5
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-24 22:33:30 +0400

    модели доработал

--- 8/HW8/models/models.php
+++ 8/HW8/models/models.php
@@ -1,4 +1,5 @@
 <?
+// Соединение с базой данных.
 function db_connect() {
 
 	$connect = mysqli_connect(HOST, MYSQL_LOGIN, MYSQL_PASS, MYSQL_DB);
@@ -12,6 +13,7 @@ function db_connect() {
 
 }
 
+// Закрытие соединения с базой данных.
 function db_close($connect) {
 
 	if (mysqli_close($connect)) {
@@ -23,43 +25,72 @@ function db_close($connect) {
 
 }
 
+// Регистрация пользователя.
 function db_user_registration($login, $pass, $date, $name) {
 	
 	$connect = db_connect();
-	$query = "INSERT INTO `users`(`login`, `password`, `date`, `name`) VALUES ('$login', '$pass', '$date', '$name')";
+	$query = "INSERT INTO ".MYSQL_TABLE."(`login`, `password`, `date`, `name`) VALUES ('$login', '$pass', '$date', '$name')";
 	
 	if ($res = mysqli_query($connect, $query)) {
-		// echo "Данные отправлены.\n\n";
+		echo "Данные отправлены.\n\n";
+		// Установка cookies.
+	  setcookie('login', $login, time()+2592000);
+		setcookie('pass', $pass, time()+2592000);
+		setcookie('name', $name, time()+2592000);
 	} 
 	else {
-		// echo "Данные не отправлены!\n\n";
+		echo "Данные не отправлены!\n\n";
 	}
 	db_close($connect);
 
 }
 
+// Достать из базы все логины, пароли и имена.
+function db_get_all_login_pass_name() {
+	$connect = db_connect();
+	$query = "SELECT `login`, `password`, `name` FROM ".MYSQL_TABLE." WHERE id>0";
+	$res = mysqli_query($connect, $query);
+	db_close($connect);
+	// var_dump($res);
+	return $res;
+}
+
+// Логин занят или нет.
 function login_is_busy_or_not($login) {
 
 	$connect = db_connect();
-	$query = "SELECT `login` FROM `users` WHERE `login`='$login'";
+	$query = "SELECT `login` FROM ".MYSQL_TABLE." WHERE `login`='$login'";
 	$res = mysqli_query($connect, $query);
 	$arrayOfLogins = array();
 	$row = mysqli_fetch_assoc($res); 
 	$loginInDbIsTheSame = $row['login'];
 	if ($loginInDbIsTheSame===$login) {
-		// echo "В базе данных есть логин: ".$login."!!!\n\n";
+		echo "В базе данных есть логин: ".$login."!!!\n\n";
 		db_close($connect);
 		return true;
 	}
 	else {
-	  // echo "В базе данных нет вашего логина.".$login."!!!\n\n";
+	  echo "В базе данных нет вашего логина.".$login."!!!\n\n";
 	  db_close($connect);
 	  return false;
 	}
 	
 }
 
+// ----------------------------------
+// Обновления страницы или редиректы.
 function refresh() {
 	header("Location: ".$_SERVER["REQUEST_URI"]."");
 	exit;
 }
+
+function refresh_index() {
+	header("Location: ../public/index.php");
+	exit;
+}
+
+function refresh_page($page_path) {
+	header("Location: $page_path");
+	exit;
+}
+// ----------------------------------
