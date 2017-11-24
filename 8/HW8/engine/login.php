4a72f77ecbb0c2b460fa2199d17d2623a0812d0a
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-24 22:30:04 +0400

    контроллер логина

---
+++ 8/HW8/engine/login.php
@@ -0,0 +1,37 @@
+<?
+// Контроллер.
+$legend_content = 'Вход';
+$login = (string)htmlspecialchars(strip_tags($_POST['login']));
+$pass = (string)htmlspecialchars(strip_tags(md5(PAPPER.$_POST['pass'].SALT)));
+
+$result = db_get_all_login_pass_name($login, $pass);
+
+if ($_POST['enter']) {
+	echo "Была нажата кнопка Войти";
+	if ($result) {
+    /* извлечение ассоциативного массива */
+    while ($row = mysqli_fetch_assoc($result)) {
+    	// echo "<pre>";			
+	      // echo $row["login"]." ";
+	      // echo $row["password"]." ";
+				// echo $row["name"]." ";	      
+			// echo "</pre>";
+	    $name = $row["name"];
+      if ($row["login"]===$login && $row["password"]===$pass) {
+      	echo "ВЫ ВВЕЛИ ВЕРНЫЕ ДАННЫЕ ";
+      	setcookie('login', $login, time()+2592000);
+				setcookie('pass', $pass, time()+2592000);
+				setcookie('name', $name, time()+2592000);
+				refresh();
+      } else {
+      	// echo "Вы ввели неверный логин и пароль.";
+      }
+    }
+    /* удаление выборки */
+    mysqli_free_result($result);
+	}
+
+}
+
+// Представление.
+require_once('../templates/login.php');
