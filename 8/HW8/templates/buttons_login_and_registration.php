535cd2e07d9ef73c335769ad9a3fb68b48700b03
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-25 19:02:46 +0400

    представление кнопок

---
+++ 8/HW8/templates/buttons_login_and_registration.php
@@ -0,0 +1,9 @@
+<form action="" method="post">
+	<br>
+	<label for="registration">Вы можете</label>
+	<input type="submit" id='registration' name="registration" value="Зарегистрироваться"> 
+	<label for="login">или</label>
+	<input type="submit" id='login' name="login" value="Войти">
+	<br>
+	<br>
+</form>
