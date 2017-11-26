a885d668060fa9df750fdae23fa7b9c3c9bb8ebd
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-26 16:25:07 +0400

    ...

--- 8/HW8/public/index.php
+++ 8/HW8/public/index.php
@@ -1,28 +1,30 @@
 <?php
+	session_start();
 	// error_reporting(E_ERROR);
 	require_once('../config/conf.php');
 	require_once('../models/models.php');
-	// session_start();
 
 	// http://php.net/manual/ru/datetime.settimezone.php
 	# 'Europe/Samara'
 	// date_default_timezone_get('Europe/Samara');
 	// echo ini_get('date.timezone');
 	// print_r(DateTimeZone::listIdentifiers());
+	echo "ADMIN? ";
+	echo $_SESSION['admin'];
 ?>
 
 <!DOCTYPE html>
 <html lang="ru">
-	<? require_once('../templates/head.php'); ?>
+	<?php require_once('../templates/head.php'); ?>
 <body>
 	<h1>Добро пожаловать в питомник шнауцеров "Монинг Стар"</h1>
 	
 	<header>
-		<? require_once('../engine/user_is_logged_or_not.php'); ?>
+		<?php require_once('../engine/user_is_logged_or_not.php'); ?>
 	</header>
 
 	<main>
-		
+		<?php require_once('../engine/all_dogs.php'); ?>
 	</main>
 
 	<footer>
