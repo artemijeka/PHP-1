a5fc8a30bef0c7f9ea917519c2e06c9458497e2e
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-26 16:27:08 +0400

    ...

--- 8/HW8/engine/user_is_login.php
+++ 8/HW8/engine/user_is_login.php
@@ -1,20 +1,19 @@
-<?
-// require_once('../models/models.php');
-// setcookie('login', $login, time()-2592000);
-// setcookie('pass', $pass, time()-2592000);
-$name = $_COOKIE['name'];
+<?php // Контроллер.
 
+$name = $_COOKIE['name'];
 // var_dump($name);
 
 if ($_POST['logOut']==="Выйти") {
+	// Удаление куки.
 	setcookie('login', $login, time()-1);
   setcookie('pass', $pass, time()-1);
   setcookie('name', $name, time()-1);
   require_once('../engine/user_is_logged_or_not.php');
-  $page_path = '../public/index.php';
-  refresh_page($page_path);
+  // $page_path = '../public/index.php';
+  refresh_page('../public/index.php');
 }
 
 // Представление.
 require_once('../templates/user_is_login.php');
 
+?>
