08938ab8e5da0cf8481a947d16e8c68a22e964d9
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-28 21:18:36 +0400

    Доработал панел админа

--- 8/HW8/admin/panel_admin.php
+++ 8/HW8/admin/panel_admin.php
@@ -1,6 +1,7 @@
 <?php // КОНТРОЛЛЕР.
 	$h3 = 'Добавить собаку.';
-	$dirAndNameUploadFile = DIR_BIG_IMG . basename( translit($_FILES['imageDog']['name']) );
+	$dirAndNameUploadImage = DIR_BIG_IMG . basename( translit($_FILES['imageDog']['name']) );
+	$dirAndNameSmallImage = DIR_SMALL_IMG . basename( translit($_FILES['imageDog']['name']) );
 	$imageDog = $_FILES['imageDog'];
 	$title = $_POST['titleDog'];
 	$description = $_POST['descriptDog'];
@@ -8,13 +9,19 @@
 	if ( isset($_POST['uploadDogImage']) ) {
 
 		$imageDog = $_FILES['imageDog'];
-		var_dump($imageDog);
-		if ( upload_image_to_server($imageDog, $dirAndNameUploadFile)) {
+		// var_dump($imageDog);
+		// Если загрузка состоялась.
+		if ( upload_image_to_server($imageDog, $dirAndNameUploadImage)) {
 			// $h3 = "Файл корректен и был успешно загружен.";
-			if ( db_add_dogs_info($dirAndNameUploadFile, $title, $description) ) {
+			resize(400, $dirAndNameSmallImage, $dirAndNameUploadImage);
+			if ( db_add_dogs_info($dirAndNameUploadImage, $title, $description) ) {
 				$h3 = "Данные о собаке были добавлены в базу данных...";
 			}
+			else {
+				$h3 = "Либо такое изображение есть, либо оно больше 5Мб!";
+			}
 		}
+
 	}
 
 	// ПРЕДСТАВЛЕНИЕ.
