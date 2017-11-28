1eb628f6d936d7f9bf5b2b02f14fa2a3af10b2c0
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-28 21:19:07 +0400

    модели доработал, чтото из интернета взял

--- 8/HW8/models/models.php
+++ 8/HW8/models/models.php
@@ -48,20 +48,54 @@ function db_get_all_login_pass_name() {
 	return $res;
 }
 
+// Загрузка изображения на сервер.
+function upload_image_to_server($uploadedImage, $pathToDir) {
+
+	if ($uploadedImage['size']<5000000) {
+
+		if (move_uploaded_file($uploadedImage['tmp_name'], $pathToDir)) {
+			// echo "Файл корректен и был успешно загружен.\n";
+			return true;
+		} else if ($uploadedImage['size']>5000000) {
+			// echo "Файл небыл загружен, потому что больше 5Мб!\n";
+			return false;
+		} 
+		
+	} 
+	 
+}
+
+// Функция добавления информации о собаке в базу данных.
 function db_add_dogs_info($pathToFile, $title, $description) {
 	
 	$connect = db_connect();
-
+	$queryForPath = "SELECT `path` FROM ".MYSQL_DOGS." WHERE id>0";
 	$query = "INSERT INTO ".MYSQL_DOGS."(`path`, `title`, `description`) VALUES ('$pathToFile', '$title', '$description')";
-	if ( mysqli_query($connect, $query) ) {
-		// echo "Данные переданы в базу данных.";
-		return true;
-	} else {
-		// echo "Данные не были переданы!";
-		return false;
-	}
+	
+	if ( $resQuery = mysqli_query($connect, $queryForPath) ) {
 
+		$arrayOfPathsToImages = array();
+		while ( $row = mysqli_fetch_row($resQuery) ) {
+			$arrayOfPathsToImages[] = $row[0];
+		}
+		// echo "<pre>";
+		// var_dump($arrayOfPathsToImages);
+		// echo "</pre>";
+		// Если путь к файлу и его имя не совпадет с путем и именем в базе данных то можно загружать новый файл.
+		if ( !$res = in_array($pathToFile, $arrayOfPathsToImages) ) {
+			// var_dump($res);
+			mysqli_query($connect, $query);
+			return true;
+		} 
+		else {
+			// echo "Данные не были переданы!";
+			return false;
+		}
+
+	}  
+		
 	db_close($connect);
+	echo "БАЗА ЗАКРЫТА!";
 
 }
 
@@ -105,28 +139,53 @@ function refresh_page($page_path) {
 }
 // ----------------------------------
 
-// Загрузка изображения на сервер.
-function upload_image_to_server($uploadedImage, $pathToDir) {
-
-	if ($uploadedImage['size']<5000000) {
-
-		if (move_uploaded_file($uploadedImage['tmp_name'], $pathToDir)) {
-			// echo "Файл корректен и был успешно загружен.\n";
-			return true;
-		} else if ($uploadedImage['size']>5000000) {
-			// echo "Файл небыл загружен, потому что больше 5Мб!\n";
-			return false;
-		} else {
-			// echo "Ошибка при загрузке файла!";
-			return false;
-		}
-	} 
-	 
-}
-
 // Транслит.
 function translit($str) {
-	$rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я');
-	$lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya');
+	$rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', ' ');
+	$lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', 'y', 'y', 'y', 'e', 'yu', 'ya', '_');
 	return str_replace($rus, $lat, $str);
+}
+
+// Функция уменьшения изображения.
+function resize($newWidth, $targetFile, $originalFile) {
+
+    $info = getimagesize($originalFile);
+    $mime = $info['mime'];
+
+    switch ($mime) {
+            case 'image/jpeg':
+                    $image_create_func = 'imagecreatefromjpeg';
+                    $image_save_func = 'imagejpeg';
+                    // $new_image_ext = 'jpg';
+                    break;
+
+            case 'image/png':
+                    $image_create_func = 'imagecreatefrompng';
+                    $image_save_func = 'imagepng';
+                    // $new_image_ext = 'png';
+                    break;
+
+            case 'image/gif':
+                    $image_create_func = 'imagecreatefromgif';
+                    $image_save_func = 'imagegif';
+                    // $new_image_ext = 'gif';
+                    break;
+
+            default: 
+                    throw new Exception('Unknown image type.');
+    }
+
+    $img = $image_create_func($originalFile);
+    list($width, $height) = getimagesize($originalFile);
+
+    $newHeight = ($height / $width) * $newWidth;
+    $tmp = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($tmp, $img, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+    if (file_exists($targetFile)) {
+            unlink($targetFile);
+    }
+    // $image_save_func($tmp, "$targetFile.$new_image_ext");
+    $image_save_func($tmp, "$targetFile");
+
 }
