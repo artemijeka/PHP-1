d650659930503a1f98ac70b9109b17533419013c
Artemije <artem.kuznecov.samara@gmail.com>
2017-11-29 22:14:26 +0400

    контроллер обрабатывающий вывод собак

--- 8/HW8/engine/all_dogs.php
+++ 8/HW8/engine/all_dogs.php
@@ -1,13 +1,32 @@
 <?php # Контроллер.
+	$res = db_get_all_paths_titles_descriptions();
+	// var_dump($res);
 	
+	// Представление.
+	require_once('../templates/all_dogs_title.tpl');
+
+	// $arrayImagesAndTitleAndDescription = array();
+	while ($row = mysqli_fetch_assoc($res)) {
+		// $arrayImagesAndTitleAndDescription[] = $row;
+
+		$currentPathMiniImage = $row['path_mini'];
+		$currentTitle = $row['title'];
+		$currentDescription = $row['description'];
+		// Возьму дирректорию страницы собаки.
+		$dirPageDog = $row['dog_page_dir'];
+		var_dump($dirPageDog);
+
+		// Представление карточки собаки в цикле.
+		include('../templates/all_dogs.tpl');
+
+	}
+	// echo "<pre>";
+	// var_dump($arrayImagesAndTitleAndDescription);
+	// echo "</pre>";
+
+/**
+ * ДОБАВИТЬ РЕДАКТИРОВАНИЕ КАРТОЧКИ НА МЕСТЕ !!!
+ */
 
 
-
-
-
-
-
-
-	# Представление.
-	require_once('../templates/all_dogs.tpl');
 ?>
